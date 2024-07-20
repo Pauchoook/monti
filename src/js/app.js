32d4a033646c3e72b6d5bdd16bba6c4ea6f6887e
Pauchoook <nkolatarasov@mail.ru>
2024-07-20 13:34:57 +0300

    end

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,10 @@ import * as functions from "./files/functions.js";
 import "../scss/style.scss";
 import spoller from "./files/spoller.js";
 import tab from "./files/tab.js";
+import smoothScroll from "./files/smoothScroll.js";
 
 functions.isWebp();
 
 spoller();
 tab();
+smoothScroll();
